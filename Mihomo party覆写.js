f0511bdfcbe7adad160d3bcd65cc3a9a98f07644
JacobLee713 <110952755+JacobLee713@users.noreply.github.com>
2025-08-18 17:59:55 +0800

    Update Mihomo party覆写.js

--- Mihomo party覆写.js
+++ Mihomo party覆写.js
@@ -225,7 +225,7 @@ function main(config) {
     "DOMAIN-SUFFIX,download.epicgames.com,DIRECT",
     "DOMAIN-SUFFIX,epicgames-download1.akamaized.net,DIRECT",
 
-    "DOMAIN-SUFFIX,dogfight360.com,DIRECT"
+    "DOMAIN-SUFFIX,dogfight360.com,DIRECT",
 
     "RULE-SET,steam,Steam",
     "RULE-SET,epic,Epic",
